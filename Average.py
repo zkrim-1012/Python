bfd0886c099ae2431efc7a5430555e1bee503e46
zkrim-1012 <atikano7783@outlook.com>
2025-12-30 21:42:36 -0500

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,13 @@
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 95
+tree5 = 11
+
+# Finding  the total of trees
+sum = tree1+tree2+tree3+tree4+tree5
+print("the sum of all the 5 trees is: ", sum)
+
+#Finding the average of trees
+average = sum/5
+print("the average of all the tree is :", average)
