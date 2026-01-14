becebc7dfe15b42089592a48ac6263ff9ba3534b
zkrim-1012 <atikano7783@outlook.com>
2026-01-13 21:46:20 -0500

    Add files via upload

---
+++ Not Equal operator.py
@@ -0,0 +1,26 @@
+a = 10
+b = 12
+c = 12
+
+print(a != b)
+print(b != c)
+
+
+a = "python"
+b = "python"
+if a != b :
+    print(a, 'and', b, 'are different')
+
+    
+    a = 4
+    a = 5
+
+    if (a == 1) != (b == 5):
+        print('Hello')
+
+
+
+        a = int(input("enter a number"))
+
+        if a%2 != 0 : 
+            print(a, "is not even number.")
