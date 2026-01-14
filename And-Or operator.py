853ff88d785e8c62a2921bd17a09ec7e90352e3b
zkrim-1012 <atikano7783@outlook.com>
2026-01-13 21:26:30 -0500

    Add files via upload

---
+++ And-Or operator.py
@@ -0,0 +1,18 @@
+a = 10
+b = 12
+c = 0 
+if a and b and c:
+    print("All expression are evaluated to True")
+else:
+    print("At least one expression is evaluated to False")
+    a=10
+    b = -10
+    c = 0
+if a > 0 or b > 0 :
+        print("Either of the expressions is evaluated to True")
+else:
+        print("No expression is evaluated to True")
+if b > 0 or c > 0:
+            print("Either of the expressions is evaluated to True")
+else:
+            print("No expression is evaluated to True")
